366cacea2b1146ed4d47136966bc9472d0db8ade
Seth Berg <sethberg@Seths-MacBook-Air.local>
2013-12-21 17:10:51 -0500

    Changed timer logic to use system clock

--- ME/src/com/google/android/glass/sample/stopwatch/ChronometerView.java
+++ ME/src/com/google/android/glass/sample/stopwatch/ChronometerView.java
@@ -23,6 +23,7 @@ import android.os.Handler;
 import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.widget.TextView;
@@ -194,10 +195,12 @@ public class ChronometerView extends FrameLayout {
     }
     
     private boolean timeToTakePicture(long millis, int interval) {
-		return millis - mLastPhotoMillis > interval;
+    	Log.d("Picture", "" + millis + ", " + mLastPhotoMillis);
+		return SystemClock.elapsedRealtime() - mLastPhotoMillis > interval;
 	}
 
 	private void takePicture() {
+		Log.d("Picture", "Take Pic");
 	    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    ((Activity)getContext()).startActivityForResult(intent, TAKE_PICTURE_REQUEST);
 	}

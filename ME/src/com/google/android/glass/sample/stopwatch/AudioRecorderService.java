dc32fea7b12af5fe40bb625b90bbc1fc256d92f4
Seth Berg <sethberg@Seths-MacBook-Air.local>
2013-12-23 11:59:51 -0500

    Added Android support library. Also have audiorecording working with AudioRecord, but haven't converted raw PCM to WAV or AAC.

--- ME/src/com/google/android/glass/sample/stopwatch/AudioRecorderService.java
+++ ME/src/com/google/android/glass/sample/stopwatch/AudioRecorderService.java
@@ -1,5 +1,7 @@
 package com.google.android.glass.sample.stopwatch;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import android.app.Service;
@@ -7,7 +9,6 @@ import android.content.Intent;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.media.MediaRecorder.AudioSource;
 import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
@@ -15,37 +16,91 @@ import android.util.Log;
 public class AudioRecorderService extends Service{
 	
 	private static final String LOG_TAG = "AudioRecordTest";
-	private MediaRecorder recorder;
-	private String mFileName = null;
-	
-	private void AudioRecordTest() {
 
-		mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += ("/" + Environment.DIRECTORY_PICTURES + "/audiorecordtest_" + String.valueOf(System.currentTimeMillis()) + ".mp4");
+	private static final int RECORDER_SAMPLERATE = 8000;
+	private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+	private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+	private AudioRecord recorder = null;
+	
+	int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+	int BytesPerElement = 2; // 2 bytes in 16bit format
+
+	private void startRecording() {
+
+	    recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+	            RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+	            RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+	    recorder.startRecording();
+	}
+
+	//convert short to byte
+	private byte[] short2byte(short[] sData) {
+	    int shortArrsize = sData.length;
+	    byte[] bytes = new byte[shortArrsize * 2];
+	    for (int i = 0; i < shortArrsize; i++) {
+	        bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+	        bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+	        sData[i] = 0;
+	    }
+	    return bytes;
+
+	}
+
+	private void writeAudioDataToFile() {
+	    // Write the output audio in byte
+
+	    String filePath = AudioFileName();
+	    short sData[] = new short[BufferElements2Rec];
+
+	    FileOutputStream os = null;
+	    try {
+	        os = new FileOutputStream(filePath);
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    }
+
+        // gets the voice output from microphone to byte format
+
+        recorder.read(sData, 0, BufferElements2Rec);
+        System.out.println("Short wirting to file" + sData.toString());
+        try {
+            // // writes the data to file from buffer
+            // // stores the voice buffer
+            byte bData[] = short2byte(sData);
+            os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+	    try {
+	        os.close();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	private void stopRecording() {
+	    // stops the recording activity
+	    if (null != recorder) {
+	        recorder.stop();
+	        recorder.release();
+	        recorder = null;
+	    }
+	}
+	
+	private String AudioFileName() {
+		String mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        mFileName += ("/" + Environment.DIRECTORY_PICTURES + "/audiorecordtest_" + String.valueOf(System.currentTimeMillis()) + ".pcm");
         Log.d(LOG_TAG, mFileName);
+        return mFileName;
     }
 
 	@Override
     public void onCreate() {
         super.onCreate();
-        Log.d(LOG_TAG, "Recording Service Beginning");
-        
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-        this.AudioRecordTest();
-        recorder.setOutputFile(mFileName);
-        try{
-        	recorder.prepare();
-        }
-        catch(IOException prepareFailed){
-        	Log.d(LOG_TAG, prepareFailed.toString());
-        	recorder = null;
-        	return;
-        }
-        recorder.start();   // Recording is now started
-        Log.d(LOG_TAG, "Recording Service Beginning");  
+        Log.d(LOG_TAG, "Recording Service onCreate Called");
+        startRecording();
     }
 
     @Override
@@ -61,14 +116,9 @@ public class AudioRecorderService extends Service{
     @Override
     public void onDestroy() {
     	Log.d(LOG_TAG, "Recording Service onDestroy Called");
-    	if(recorder != null){
-	    	recorder.stop();
-	        recorder.reset();   // You can reuse the object by going back to setAudioSource() step
-	        recorder.release(); // Now the object cannot be reused
-	        Log.d(LOG_TAG, "Recording Stopped");
-    	}
+    	writeAudioDataToFile();	//I wonder if these two lines need to be flipped.
+    	stopRecording();
         super.onDestroy();
-        
     }
 }
 

24af328a1c8c2ac9948f6d75e9adc4a524dbf851
Seth Berg <sethberg@mit.edu>
2013-12-26 16:58:02 -0500

    Issue ith timelinemanager api not recognized.

--- Memora/src/com/example/memora/AudioRecorder.java
+++ Memora/src/com/example/memora/AudioRecorder.java
@@ -1,12 +1,43 @@
 package com.example.memora;
 
 
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.TimelineManager;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
 public class AudioRecorder extends Service {
+	
+	private LiveCard mLiveCard;
+
+	private void publishCard(Context context) {
+	    if (mLiveCard == null) {
+	        String cardId = "my_card";
+	        TimelineManager tm = TimelineManager.from(context);
+	        mLiveCard = tm.getLiveCard(cardId);
+
+	        mLiveCard.setViews(new RemoteViews(context.getPackageName(), R.layout.card_text));
+	        Intent intent = new Intent(context, EntryActivity.class);
+	        mLiveCard.setAction(PendingIntent.getActivity(context, 0,
+	                intent, 0));
+	        mLiveCard.publish();
+	    } else {
+	        // Card is already published.
+	        return;
+	    }
+	}
+
+	private void unpublishCard(Context context) {
+	    if (mLiveCard != null) {
+	        mLiveCard.unpublish();
+	        mLiveCard = null;
+	    }
+	}
+	
 	public AudioRecorder() {
 	}
 	

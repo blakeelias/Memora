66f4db51853c378810b4bfc60fd606ba1983503e
Blake Elias <blakeelias@gmail.com>
2014-01-25 12:45:50 -0500

    Move timeline interface into Meteor project

---
+++ server/timeline/client/slider.js
@@ -0,0 +1,30 @@
+if (Meteor.isClient) {
+	$(document).ready(function()
+	{
+		var knob = $( "#slider_knob" );
+		var slider = $( "#slider_body" );
+		
+		var sliderLeft = 0;
+		var knobLeft = 0;
+		var position = 0;
+		knob.draggable(
+		{
+			containment: "parent",	
+			axis: "x",
+			drag: function() 
+			{
+	        	sliderLeft = slider.offset().left;
+				knobLeft = knob.offset().left;
+				knobRel = knobLeft - sliderLeft;
+				position = knobRel / (slider.width() - knob.width()/2);
+				/*$(this).text(String(position));*/
+			}
+		});
+
+		$(window).resize(function(){
+			var newPos = slider.offset().left + slider.width()*position;
+			knob.offset({ left: newPos});
+		});
+
+	});
+}

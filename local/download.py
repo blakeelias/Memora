095714ae24b1a447319bc6571ae7e5cb34338cb2
Blake Elias <blakeelias@gmail.com>
2014-07-18 22:31:30 -0400

    Replace largePhotoPath with getOriginalPath() - small fix

--- local/download.py
+++ local/download.py
@@ -82,7 +82,7 @@ def pull_auto_photos(auto_photo_path, local_photo_path):
 	print('pull_auto_photos()')
 	print('-'*30)
 	call(['mkdir', getOriginalPath()])
-	call([ADB_PATH, "pull", auto_photo_path, largePhotoPath])
+	call([ADB_PATH, "pull", auto_photo_path, getOriginalPath()])
 
 def copy_file_and_process(remote_photo_path, f, local_photo_path):
 	call([ADB_PATH, "pull", remote_photo_path + f, local_photo_path])

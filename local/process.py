e60d76cfe6bd195a3039016f4a855ebc850821b6
Blake Elias <blakeelias@gmail.com>
2014-08-15 20:48:31 -0400

    Add function standardizedName(...) for renaming files when they are moved. Give each photo a standardized timestamp format as well as a tag for being manually taken.

--- local/process.py
+++ local/process.py
@@ -6,16 +6,38 @@ from download import get_local_file_path, getOriginalPath
 
 def main():
     
-    for photoName in check_output(['ls', getOriginalPath()]).split('\n'):
-        photoName = photoName.split('\r')[0]
-        time = getTime(photoName)
-        makeSizes(getOriginalPath() + '/' + photoName, get_local_file_path(), {'thumbs': 158, 'previews': 632}, newSubPath(time))
+    for line in check_output(['ls', getOriginalPath()]).split('\n'):
+        photoFileName = line.split('\r')[0]
+        (prefix, suffix) = photoFileName.split('.')
+        time, manual = getTime(prefix)
+        makeSizes(getOriginalPath() + '/' + photoFileName,
+            get_local_file_path(),
+            {'thumbs': 158, 'previews': 632},
+            newSubPath(time),
+            standardizedName(time, suffix, manual))
 
 
-def getTime(fileName):
-    millis = int(fileName.split('.')[0])
-    time = datetime.fromtimestamp(millis / 1000.0)
-    return time
+def getTime(filePrefix):
+    '''Return the time indicated by a file name's prefix string (i.e., everything before the file extension).
+        Eg. for the file naemed '20140729_081534_016.jpg', you would call:
+
+        >>> getTime('20140729_081534_016')
+        datetime.datetime(2014, 7, 29, 8, 15, 34, 16000)
+    '''
+    manual = '_' in filePrefix
+    if manual:
+        chunks = filePrefix.split('_')
+        if len(chunks) == 3:
+            # Indicates Android photo names, eg. "20140729_081534_016.jpg"
+            
+            # put three trailing 0s to convert milliseconds (10^-3 s) into microseconds (10^-6 s)
+            timeWithMicros = filePrefix + '000'
+            time = datetime.strptime(timeWithMicros, '%Y%m%d_%H%M%S_%f')
+    else:
+        millis = int(filePrefix)
+        time = datetime.fromtimestamp(millis / 1000.0)
+    
+    return time, manual
 
 def getFileName(path):
     return path.split('/')[-1]
@@ -35,28 +57,36 @@ def newSubPath(time):
 def getPath(photo):
     return '/'.join(photo.split('/')[:-1])
 
-def makeSizes(photoPath, dirPathToSave, widths, indexDir):
+def makeSizes(photoPath, dirPathToSave, widths, indexDir, newName):
     for name in widths:
         makeResize(
             photoPath,
             dirPathToSave + '/' + name + '/' + indexDir,
-            widths[name])
+            widths[name],
+            newName)
     newDir = dirPathToSave + 'large/' + indexDir
     call(['mkdir', '-p', newDir])
-    call(['mv', photoPath, newDir])
+    call(['mv', photoPath, newDir + '/' + newName])
 
-def makeResize(photoPath, resizePath, width):
-    print('resizing photo: ' + photoPath)
+def makeResize(photoPath, resizePath, width, newName):
+    #print('resizing photo: ' + photoPath)
     try:
         im = Image.open(photoPath)
         size = (width, im.size[1] * (width*1.0 / im.size[0]))
         im.thumbnail(size, Image.ANTIALIAS)
-        name = getFileName(photoPath)
         call(['mkdir', '-p', resizePath])
-        im.save(resizePath + '/' + name, "JPEG")
+        newPath = resizePath + '/' + newName
+        im.save(newPath, "JPEG")
+        print('resized photo: %s' % newPath)
     except Exception as e:
         print('exception in resizing file: ' + photoPath)
         print(e)
+
+def standardizedName(time, suffix, manual):
+    # requires Python version >= 3.3
+    #return str(int(time.timestamp())) + '.' + suffix
+
+    return time.strftime('%Y%m%d_%H%M%S_%f' + ('manual' if manual else '')) + '.' + suffix
 
 if __name__ == '__main__':
     main()
